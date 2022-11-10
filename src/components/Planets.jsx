5d9f549d48809d671a6bd140ebc105b91ea44d54
Ayomide Bajo <oluwashinabajo@gmail.com>
2022-11-10 16:32:07 +0100

    add use inifinite hook

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,23 +1,61 @@
 import React from "react";
-import { useQuery } from "react-query";
+import { useState } from "react";
+import { useQuery, useInfiniteQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanetsData = async () => {
-  const res = await fetch("http://swapi.dev/api/planets");
+const fetchPlanetsData = async (key) => {
+  //   console.log(key, "great");
+  const res = await fetch(
+    `http://swapi.dev/api/planets/?page=${key.queryKey[2]}`
+  );
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planetsData", fetchPlanetsData);
-  console.log(data, "data");
+  const [page, setPage] = useState(1);
+
+  const fetchPlanetsDataInifinity = async (page) => {
+    //   console.log(pageParam, "hh");
+    const res = await fetch(
+      `http://swapi.dev/api/planets/?page=${page.queryKey[1]}`
+    );
+    return res.json();
+  };
+  //   useInfiniteQuery
+  const { data, status, isPreviousData } = useInfiniteQuery(
+    ["planetsData", page],
+    fetchPlanetsDataInifinity,
+    {
+      getNextPageParam: (lastPage, pages) => {
+        // if (lastPage.next) {
+        //   return page;
+        // }
+        return page;
+      },
+    }
+  );
+
+  // useQuery
+  //   console.log(data, "data");
   return (
     <>
-      <div>Planets</div>
-      {/* <p>{status}</p>{" "} */}
+      <div>Planets - Page {page}</div>
+      <button
+        disabled={page === 1}
+        onClick={() => setPage((old) => Math.max(old - 1, 1))}
+      >
+        Previous Page
+      </button>
+      <button
+        disabled={page * 10 === data?.pages[0].count}
+        onClick={() => setPage((old) => Math.max(old + 1, 1))}
+      >
+        Next Page
+      </button>
       {status === "loading" && <div className="">Loading data...</div>}
       {status === "success" && (
         <div className="">
-          {data.results.map((item) => (
+          {data?.pages[0]?.results?.map((item) => (
             <Planet key={item.name} planet={item} />
           ))}
         </div>
